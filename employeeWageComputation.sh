7bf1a722e1700a8d44fa35385c84647161575ae9
anish <anishsalgia1997@gmail.com>
2020-12-10 16:51:46 +0530

    ADD: Added EmployeeAttendance code

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,1 +1,10 @@
+#!/bin/bash -x
 echo "Welcome to EmployeeWage Computation"
+randomCheck=$((RANDOM%2))
+isPresent=1
+if [ $isPresent -eq $randomCheck ]
+then
+        echo "Employee is Present"
+else
+        echo "Employee is Absent"
+fi
